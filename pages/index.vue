514160cd9770efcea9c5e2338aa75ecf369500ae
urayusuke <blue.yusuke69@gmail.com>
2023-01-25 21:54:51 +0900

    不要なファイルの削除

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,17 @@
-<template>  
-  <v-app id="app">
-    <nuxt />
-  </v-app>
+<template>
+  <v-row justify="center">
+    <v-col cols="12" md="8">
+      <p class="display-1">{{ title }}</p>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-  export default {
-    //
+export default {
+  data() {
+    return {
+      title: ''
+    }
   }
+}
 </script>
---
+++ pages/login.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-card>
+    <v-card-title>ログイン</v-card-title>
+    <v-card-text></v-card-text> 
+  </v-card>
+</template>
