bb10d98de5d57ad08ef1542a7c596c40f667af05
urayusuke <blue.yusuke69@gmail.com>
2023-01-28 10:24:58 +0900

    test

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,27 @@
 <template>
   <v-card>
     <v-card-title>ログイン</v-card-title>
-    <v-card-text></v-card-text> 
+    <v-card-text>
+      <form>
+    <v-text-field
+
+    ></v-text-field>
+    <v-text-field
+
+    ></v-text-field>
+    <v-select
+    ></v-select>
+    <v-checkbox
+    ></v-checkbox>
+
+    <v-btn
+    >
+      submit
+    </v-btn>
+    <v-btn>
+      clear
+    </v-btn>
+  </form> 
+    </v-card-text> 
   </v-card>
 </template>
